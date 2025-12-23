8c98b02a2373634fac496ed647e30d2e97c6f7be
mayconullrich <maycon123@gmail.com>
2025-12-22 23:44:11 -0300

    Start migration: upgrade to Spring Boot 3.5.0

---
+++ src/test/java/com/sistema/fluxo/FluxoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.fluxo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
